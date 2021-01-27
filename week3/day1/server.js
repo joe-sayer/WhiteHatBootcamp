0fe1df37bf171c02fe183a81b8dc570746f274ff
joe-sayer <sayerj97@gmail.com>
2021-01-27 13:45:33 +0000

    updated routes

--- week3/day1/server.js
+++ week3/day1/server.js
@@ -4,6 +4,7 @@ const expressHandlebars = require('express-handlebars')
 const {allowInsecurePrototypeAccess} = require('@handlebars/allow-prototype-access')
 const {Restaurant} = require('./Restaurant')
 const {Menu} = require('./Menu')
+const {MenuItem} = require('./MenuItem')
 
 const app = express()
 const port = 3000
@@ -15,14 +16,35 @@ const handlebars = expressHandlebars({
 app.engine('handlebars', handlebars)
 app.set('view engine', 'handlebars')
 
+
+// root restaurants page
 app.get('/', async (req, res) => {
     const restaurants = await Restaurant.findAll({
-        include: [{model: Menu, as: 'menus'}],
+        include: [
+            {
+                model: Menu, as: 'menus',
+                include: [{model:MenuItem, as: 'items'}]
+            }
+            
+        ],
         nest: true
     })
     res.render('restaurants', {restaurants})
 })
 
+
+// dynamic routing for each restaurant displayed in restaurants page
+app.get('/restaurants/:id', async (req, res) => {
+    const restaurant = await Restaurant.findByPk(req.params.id)
+    const menus = await restaurant.getMenus({
+        include: [{model: MenuItem, as: 'items'}],
+        nest: true
+    })
+    res.render('restaurant', {restaurant, menus})
+})
+
+
+// about page
 app.get('/about', (req, res) => {
     res.render('about',
     {
@@ -33,6 +55,8 @@ app.get('/about', (req, res) => {
     })
 })
 
+
+// listen on port 3000
 app.listen(port, () => {
     console.log(`Server listening on Port ${port}...`)
 })
